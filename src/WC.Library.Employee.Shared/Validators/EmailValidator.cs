1526e53bcfc0d851848c00755fc6aac0028af3f3
SaintEfim <saintefim2@gmail.com>
2024-06-14 17:02:02 +0300

    added new validator

---
+++ src/WC.Library.Employee.Shared/Validators/EmailValidator.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using WC.Library.Shared.Constants;
+
+namespace WC.Library.Employee.Shared.Validators;
+
+public class EmailValidator : AbstractValidator<string>
+{
+    public EmailValidator(string propertyName)
+    {
+        ClassLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(x => x)
+            .NotEmpty()
+            .WithName(propertyName);
+
+        RuleFor(x => x)
+            .MinimumLength(CommonConstants.GenericEmailMinLength)
+            .WithName(propertyName);
+
+        RuleFor(x => x)
+            .MaximumLength(CommonConstants.GenericEmailMaxLength)
+            .WithName(propertyName);
+
+        RuleFor(x => x)
+            .Matches(@"^\S*$")
+            .WithName(propertyName);
+
+        RuleFor(x => x)
+            .EmailAddress()
+            .WithName(propertyName);
+
+        RuleFor(x => x)
+            .Custom((email, context) =>
+            {
+                var domain = email.Split('@').LastOrDefault();
+
+                var allowedDomains = new List<string>
+                {
+                    "gmail.com",
+                    "mail.ru",
+                    "yahoo.com",
+                    "hotmail.com",
+                    "outlook.com",
+                    "yandex.ru",
+                    "protonmail.com",
+                    "aol.com",
+                    "icloud.com",
+                    "zoho.com"
+                };
+
+                if (domain != null && !allowedDomains.Contains(domain))
+                {
+                    context.AddFailure($"Invalid domain '{domain}'");
+                }
+            });
+    }
+}
---
+++ src/WC.Library.Employee.Shared/Validators/PasswordValidator.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using WC.Library.Shared.Constants;
+
+namespace WC.Library.Employee.Shared.Validators;
+
+public class PasswordValidator : AbstractValidator<string>
+{
+    public PasswordValidator(string propertyName)
+    {
+        ClassLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(x => x)
+            .NotEmpty()
+            .WithName(propertyName);
+
+        RuleFor(x => x)
+            .MinimumLength(CommonConstants.GenericPasswordMinLength)
+            .WithName(propertyName);
+
+        RuleFor(x => x)
+            .MaximumLength(CommonConstants.GenericPasswordMaxLength)
+            .WithName(propertyName);
+
+        RuleFor(x => x)
+            .Matches(@"(?=.*\d)")
+            .WithName(propertyName)
+            .WithMessage($"{propertyName} must contain at least one digit.");
+
+        RuleFor(x => x)
+            .Matches("(?=.*[a-z])")
+            .WithName(propertyName)
+            .WithMessage($"{propertyName} must contain at least one lowercase letter.");
+
+        RuleFor(x => x)
+            .Matches("(?=.*[A-Z])")
+            .WithName(propertyName)
+            .WithMessage($"{propertyName} must contain at least one uppercase letter.");
+
+        RuleFor(x => x)
+            .Matches("[!@#$%^&*()-+=]")
+            .WithName(propertyName)
+            .WithMessage($"{propertyName} must contain at least one special character.");
+
+        RuleFor(x => x)
+            .Matches(@"^\S*$")
+            .WithName(propertyName)
+            .WithMessage($"{propertyName} cannot contain whitespace characters.");
+    }
+}
