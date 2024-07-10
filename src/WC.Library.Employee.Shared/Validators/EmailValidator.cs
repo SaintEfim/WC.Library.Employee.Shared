7e8509858dd4b088f1ab7b2195d3bd406abdfa60
SaintEfim <saintefim2@gmail.com>
2024-07-10 12:57:25 +0300

    feature/update_validators

--- src/WC.Library.Employee.Shared/Validators/EmailValidator.cs
+++ src/WC.Library.Employee.Shared/Validators/EmailValidator.cs
@@ -1,6 +1,4 @@
 ﻿using FluentValidation;
-using WC.Library.Employee.Shared.ConstantsRegex;
-using WC.Library.Shared.Constants;
 
 namespace WC.Library.Employee.Shared.Validators;
 
@@ -15,47 +13,8 @@ public class EmailValidator : AbstractValidator<string>
             .WithName(propertyName);
 
         RuleFor(x => x)
-            .MinimumLength(CommonConstants.GenericEmailMinLength)
+            .Matches(
+                @"(([^<>()\[\]\\.,;:\s@""]+(\.[^<>()\[\]\\.,;:\s@""]+)*)| copy         ("".+""))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))")
             .WithName(propertyName);
-
-        RuleFor(x => x)
-            .MaximumLength(CommonConstants.GenericEmailMaxLength)
-            .WithName(propertyName);
-
-        RuleFor(x => x)
-            .Matches(CommonConstantsRegex.GenericNoWhitespaceRegex)
-            .WithName(propertyName)
-            .WithMessage($"{propertyName} cannot contain whitespace characters.");
-
-        RuleFor(x => x)
-            .Must(x => x.IndexOf(CommonConstants.GenericAtSymbol) != -1 && x.IndexOf(CommonConstants.GenericAtSymbol) ==
-                x.LastIndexOf(CommonConstants.GenericAtSymbol))
-            .WithName(propertyName)
-            .WithMessage($"{propertyName} must contain exactly one '@' symbol.");
-
-        RuleFor(x => x)
-            .Custom((email, context) =>
-            {
-                var domain = email.Split(CommonConstants.GenericAtSymbol).LastOrDefault();
-
-                var allowedDomains = new List<string>
-                {
-                    "gmail.com",
-                    "mail.ru",
-                    "yahoo.com",
-                    "hotmail.com",
-                    "outlook.com",
-                    "yandex.ru",
-                    "protonmail.com",
-                    "aol.com",
-                    "icloud.com",
-                    "zoho.com"
-                };
-
-                if (domain != null && !allowedDomains.Contains(domain))
-                {
-                    context.AddFailure($"Invalid domain '{domain}'");
-                }
-            });
     }
 }
